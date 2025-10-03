7b894f0316eff85c199db5cbd39e3fe2b00fb1a1
MoKho <mim.khosravi@gmail.com>
2025-10-02 18:39:37 -0700

    Fix: Added wildcard CORS temporarily

--- app/main.py
+++ app/main.py
@@ -13,6 +13,9 @@ origins = [
     "http://localhost",       # For local development
     "http://localhost:3000",  # Default for Next.js dev server
     "https://aistudio.google.com/apps/drive/1OMMs_Qxb1qI24GqapxALM1nrKwNTgywj", # Google AI Studio
+    "http://localhost:3000",  # For local Next.js development
+    "https://2ln1bx7hzfopwqw021gl6ih56sbcopkebkobr14kqimqvyawx7-h813239537.scf.usercontent.goog", # Your AI Studio URL
+    "*" # A wildcard to allow any origin for now
     # Add your future frontend production URL here, e.g., "https://www.your-frontend-app.com"
 ]
 app.add_middleware(
